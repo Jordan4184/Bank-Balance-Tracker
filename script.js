fbc1b12b0d3fbc1a56be407ace71fd94a25053e9
Jordan4184 <jordandonaldson14@gmail.com>
2024-08-22 12:10:35 -0700

    Updated call functionality

--- script.js
+++ script.js
@@ -9,26 +9,17 @@ let addIncome = (function (amount, description, type){
     balance.push({amount: amount, description: description, type: type});
 }); 
 
-addIncome(5, 'Cash Deposit', 'income')
-
 //Add Expense
 let addExpense = (function (amount, description, type){
     balance.push({amount: amount, description: description, type: type})
 })
 
-addExpense(-5, 'cards', 'expense');
-
 //View Balance 
 let viewBalance = (function (){
     return balance.reduce(function (accumulator, currentValue){
-        return accumulator + currentValue.amount;
+       return accumulator + currentValue.amount;
     }, 0);
 })
-
-console.log(viewBalance());
-
-//View Tranactions
-console.log(balance);
 
 //Remove Transaction
 let removeTransaction = function(description){
@@ -37,5 +28,12 @@ let removeTransaction = function(description){
     })
 };
 
+addIncome(5, 'Cash Deposit', 'income')
+
+addExpense(-5, 'cards', 'expense');
+
 removeTransaction('cards');
+
 console.log(balance)
+let balanceTotal = viewBalance();
+console.log(`Your balance is $${balanceTotal}`);
